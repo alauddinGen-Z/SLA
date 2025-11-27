8aa263254c94f455d94aeb16fc8ae480ab5567b7
alauddinGen-Z <alauddinGen-Z@github.com>
2025-11-27 13:27:47 +0900

    chore: trigger supabase functions deployment

--- supabase/functions/paralegal/index.ts
+++ supabase/functions/paralegal/index.ts
@@ -1,4 +1,5 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+// Trigger deployment
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 import pdf from 'npm:pdf-parse/lib/pdf-parse.js'
 
